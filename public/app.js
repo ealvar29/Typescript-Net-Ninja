6401bbbeeb13a7dab2eb92eebdde58eab3ed49da
Eduardo <ealvarez@carminaticonsulting.com>
2021-06-12 14:38:32 -0500

    adding html layout now

--- public/app.js
+++ public/app.js
@@ -1,32 +1,19 @@
-"use strict";
-// Classes
-var Invoice = /** @class */ (function () {
-    function Invoice(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes $" + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-// New Instance of Invoice Class
-var invoiceOne = new Invoice("Mario", "work on the mario website", 100);
-var invoiceTwo = new Invoice("Luigi", "work on luigis website", 200);
-//Array that only allows Invoice objects
-var invoices = [];
-invoices.push(invoiceOne);
-invoices.push(invoiceTwo);
-invoices.forEach(function (inv) {
-    console.log(inv.client, inv.amount, inv.format());
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+let docOne;
+let docTwo;
+docOne = new Invoice("Yoshi", "Web Work", 350);
+docTwo = new Payment("Mario", "plumbing work", 200);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
 });
-var form = document.querySelector(".new-item-form");
-var type = document.querySelector("#type");
-var tofrom = document.querySelector("#tofrom");
-var details = document.querySelector("#details");
-var amount = document.querySelector("#amount");
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+const form = document.querySelector(".new-item-form");
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,27 @@
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { hasFormatter } from "./interfaces/hasFormatter.js";
+
+let docOne: hasFormatter;
+let docTwo: hasFormatter;
+
+docOne = new Invoice("Yoshi", "Web Work", 350);
+docTwo = new Payment("Mario", "plumbing work", 200);
 
 // New Instance of Invoice Class
 const invoiceOne = new Invoice("Mario", "work on the mario website", 100);
 const invoiceTwo = new Invoice("Luigi", "work on luigis website", 200);
+
+let docs: hasFormatter[] = [];
+docs.push(docOne);
+docs.push(docTwo);
 
 //Array that only allows Invoice objects
 let invoices: Invoice[] = [];
 invoices.push(invoiceOne);
 invoices.push(invoiceTwo);
 
+console.log(docs);
 invoices.forEach((inv) => {
   console.log(inv.client, inv.amount, inv.format());
 });
